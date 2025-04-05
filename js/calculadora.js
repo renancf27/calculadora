4217dc25d621f5bbf5e8f385983e49aef2953b7f
renancf27 <renan.filgueiras@projecao.br>
2025-04-05 19:37:45 -0300

    Add files via upload

---
+++ js/calculadora.js
@@ -0,0 +1,18 @@
+function numero(num){
+    resultado = document.getElementById("resultado");
+    if(num==='='){
+        resultado.value = eval(resultado.value);
+    } else {
+        resultado.value = resultado.value+num;
+    }
+}
+
+function operacao(operacao){
+    resultado = document.getElementById("resultado");
+    resultado.value = resultado.value + operacao;
+}
+
+function limpar(){
+    resultado = document.getElementById("resultado");
+    resultado.value = null;
+}
